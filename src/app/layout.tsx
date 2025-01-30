d0809caacbf3d03ba1aaadab0be74e94a7d5fa7f
UnzilaAli12 <unzila.aliasghar@gmail.com>
2025-01-31 00:23:52 +0500

    Fixed unused variables error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,10 @@ import Footer from "./components/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
   weight: "100 900",
 });
 
