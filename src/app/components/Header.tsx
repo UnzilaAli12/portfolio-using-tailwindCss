811a55238c6f94f34c6d257d134af99a9540f43a
UnzilaAli12 <unzila.aliasghar@gmail.com>
2025-01-31 14:19:25 +0500

    Updated layout.tsx and fixed build errors

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,12 +4,12 @@ import Link from 'next/link';
 const Header = () => {
       return (
 
-        <header className='w-full bg-gray-900 text-white py-4 px-6 fixed shadow-md ' >
-<div className='max-w-6xl mx-auto flex items-center justify-between text-2xl font-bold ' >
+        <header className='w-full bg-gray-900 text-white py-4 px-2 md:px-6 fixed shadow-md ' >
+<div className=' mx-auto flex items-center justify-between text-3xl font-bold ' >
     <h1 className='fancy-logo text-3xl font-extrabold bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500 text-transparent bg-clip-text tracking-wide   ' >
 Unzila Ali
     </h1>
-    <div className='space-x-6 text-lg ' >
+    <div className='space-x-6 text-base md:text-lg ' >
 
 <Link href="/" target='block' className='hover:text-gray-400'>Home</Link>
 <Link href="/About" target='block' className='hover:text-gray-400'>About</Link>
